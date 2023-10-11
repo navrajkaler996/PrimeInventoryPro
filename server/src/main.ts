2f00b1e30f26a21a18957fdfeb3ac7cf01ea8dc5
Navraj singh kaler <navrajkaler996@gmail.com>
2023-10-10 22:16:55 -0500

    env change

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const PORT = process.env.PORT;
+
   app.enableCors();
 
   const config = new DocumentBuilder()
@@ -16,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
