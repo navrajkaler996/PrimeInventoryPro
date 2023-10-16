eff1e8393559c74628e4866a07d0c6c32eaf9f02
Navraj singh kaler <navrajkaler996@gmail.com>
2023-10-16 16:08:32 -0500

    updated

--- client/src/hooks/useProduct.tsx
+++ client/src/hooks/useProduct.tsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
 import { ProductURLDirector } from "./helpers/ProductURLBuilder";
 import { ProductDataType } from "../pages/Dashboard/utils/types";
-import { createResponseMessage } from "./helpers/createResponseMessage";
+
+const createResponseMessage = (type: string, method: string): string => {
+  if (type === "success") {
+    if (method === "POST") return "Product created successfully";
+    if (method === "PUT") return "Product updated successfully";
+    if (method === "DELETE") return "Product deleted successfully";
+  } else if (type === "failed") {
+    if (method === "POST") return "Product could not be created!";
+    if (method === "PUT") return "Product could not be updated";
+    if (method === "DELETE") return "Product could not be deleted";
+  }
+
+  return "Unknown response";
+};
 
 const useProduct = () => {
   const [loading, setLoading] = useState(false);
