12d74724e4dd04d192fdeb86ccc0f986df4cd3ad
Navraj singh kaler <navrajkaler996@gmail.com>
2023-10-16 15:53:39 -0500

    Open/Close principle applied to useProduct

---
+++ client/src/hooks/helpers/CreateResponseMessage.ts
@@ -0,0 +1,15 @@
+const createResponseMessage = (type: string, method: string) => {
+  if (type === "success") {
+    if (method === "POST") return "Product created successfully";
+    if (method === "PUT") return "Product updated successfully";
+    if (method === "DELETE") return "Product deleted successfully";
+  } else if (type === "failed") {
+    if (method === "POST") return "Product could not be created!";
+    if (method === "PUT") return "Product could not be updated";
+    if (method === "DELETE") return "Product could not be deleted";
+  }
+
+  return "Unknown response";
+};
+
+export default createResponseMessage;
