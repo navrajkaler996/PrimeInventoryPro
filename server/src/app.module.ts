7bb2458fb8c71fa803b94a9eedc3601a82c0b4b7
Navraj singh kaler <navrajkaler996@gmail.com>
2023-10-21 13:57:17 -0500

    routing working for now

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,7 +7,6 @@ import { SubdepartmentModule } from './modules/subdepartment/subdepartment.modul
 import { ProductsModule } from './modules/products/products.module';
 import { EmployeeModule } from './modules/employee/employee.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { CatchAllModule } from './modules/catch-all/catch-all.module';
 
 @Module({
   imports: [
@@ -17,7 +16,6 @@ import { CatchAllModule } from './modules/catch-all/catch-all.module';
     ProductsModule,
     EmployeeModule,
     AuthModule,
-    CatchAllModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- server/src/modules/catch-all/catch-all.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatchAllService } from './catch-all.service';
-import { CatchAllController } from './catch-all.controller';
-
-@Module({
-  controllers: [CatchAllController],
-  providers: [CatchAllService],
-})
-export class CatchAllModule {}
