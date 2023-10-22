7d19a4f57e9cc3dbc4293e0c4c2167fa6f267b11
Navraj singh kaler <navrajkaler996@gmail.com>
2023-10-21 22:10:21 -0500

    redirecting properly

---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,35 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+import jwt_decode from "jwt-decode";
+const checkIfTokenValid = (token: string) => {
+  const decodedToken = jwt_decode(token);
+
+  if (!decodedToken) return false;
+
+  const expirationTime = decodedToken?.exp;
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  if (currentTime > expirationTime) return false;
+
+  return true;
+};
+
+const PrivateRoutes = () => {
+  const token = localStorage.getItem("accessToken");
+  let isLoggedIn;
+  if (token) isLoggedIn = checkIfTokenValid(token);
+  else isLoggedIn = false;
+
+  return isLoggedIn ? (
+    <Outlet />
+  ) : (
+    <Navigate
+      to="/login"
+      state={{
+        message: "Token expired! Please login",
+      }}
+    />
+  );
+};
+
+export default PrivateRoutes;
