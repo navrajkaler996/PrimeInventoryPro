0ace6c2b63d9669741a247dbb3a9c7c87a2b95c2
Navraj singh kaler <navrajkaler996@gmail.com>
2024-02-13 00:59:27 -0600

    done

--- client/src/pages/Dashboard/components/TopSellings.tsx
+++ client/src/pages/Dashboard/components/TopSellings.tsx
@@ -36,18 +36,17 @@ const TopSellings: React.FC = () => {
       id="dashboard__top-sellings"
       className="flex justify-center mt-[4rem] lg:mt-[0] lg:ml-[4rem]">
       <div
-        className="max-w-[35rem] w-[35rem] h-[38rem] bg-white rounded-custom shadow-custom"
+        className="max-w-[35rem] w-[30rem] h-[38rem] bg-white rounded-custom shadow-custom"
         style={topSellingIsLoading ? SKELETON_STYLES : {}}>
         <h1 className="ml-[2rem] pt-[1rem] text-[2rem]">
           Top selling in {currentDepartment?.department_name}
         </h1>
         <hr className="m-[1.5rem] text-gray" />
-        <div>
+        <div className="padding: 1rem">
           {!topSellingIsLoading && topSellingData?.length > 0 && (
             <Pie
               data={createTopSellingData(topSellingData)}
               options={options}
-              style={{ padding: ".5rem 0 1rem 1rem" }}
             />
           )}
 
